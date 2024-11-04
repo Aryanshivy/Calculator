43a264a7ca6d42f3e7c8e12e4e3f581dec23cf22
Aryanshivy <arianostrade007@gmail.com>
2024-11-04 14:10:34 +0530

    yeah

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+  const calculate = () => {
+      const evalResult = eval(input);
+      setResult(evalResult);
+  };
+  const clearInput = () => {
+    setInput("");
+    setResult("");
+  };
 
   return (
+    <div className='body'>
     <div className="App">
-      <header className="App-header">
-        <h1>DigiTIME</h1>
-        <p>{time}</p>
-      </header>
+      <h1>MathSKILLS</h1>
+      <div className="calculator">
+        <div className='flex'>
+        <input type="text" value={input} />
+        <div className="result">{result}</div>
+        </div>
+        <div className="buttons">
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button className="function2" onClick={clearInput}>C</button>
+          
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button className="function" onClick={() => handleClick("+")}>+</button>
+          
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button className="function" onClick={() => handleClick("*")}>*</button>
+          
+          <button onClick={() => handleClick("0")}>0</button>
+          <button className="function2" onClick={calculate}>=</button>
+          <button className="function" onClick={() => handleClick("-")}>-</button>
+          <button className="function" onClick={() => handleClick("/")}>/</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
